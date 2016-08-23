8a7346114da79129ab36382f6dd4fe4f68f199a5
JeffRuLz <yomanrulz@hotmail.com>
2016-08-22 21:42:07 -0500

    Add files via upload

---
+++ LegoIsland2Patcher/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace LegoIsland2Patcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.LinkLabel linkLabel1;
+            this.btnApply = new System.Windows.Forms.Button();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.clbPatches = new System.Windows.Forms.CheckedListBox();
+            this.rtbDescription = new System.Windows.Forms.RichTextBox();
+            linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new System.Drawing.Point(491, 277);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new System.Drawing.Size(138, 13);
+            linkLabel1.TabIndex = 6;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "www.rockraidersunited.com";
+            linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // btnApply
+            // 
+            this.btnApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnApply.Location = new System.Drawing.Point(12, 296);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(75, 23);
+            this.btnApply.TabIndex = 0;
+            this.btnApply.Text = "Apply";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(407, 301);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(222, 13);
+            this.linkLabel2.TabIndex = 8;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "www.github.com/JeffRuLz/LI2-Mod-Manager";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // clbPatches
+            // 
+            this.clbPatches.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.clbPatches.FormattingEnabled = true;
+            this.clbPatches.HorizontalScrollbar = true;
+            this.clbPatches.Location = new System.Drawing.Point(12, 12);
+            this.clbPatches.Name = "clbPatches";
+            this.clbPatches.Size = new System.Drawing.Size(465, 199);
+            this.clbPatches.TabIndex = 9;
+            this.clbPatches.SelectedIndexChanged += new System.EventHandler(this.clbPatches_SelectedIndexChanged);
+            // 
+            // rtbDescription
+            // 
+            this.rtbDescription.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbDescription.Location = new System.Drawing.Point(12, 242);
+            this.rtbDescription.Name = "rtbDescription";
+            this.rtbDescription.ReadOnly = true;
+            this.rtbDescription.Size = new System.Drawing.Size(465, 48);
+            this.rtbDescription.TabIndex = 10;
+            this.rtbDescription.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 331);
+            this.Controls.Add(this.rtbDescription);
+            this.Controls.Add(this.clbPatches);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(linkLabel1);
+            this.Controls.Add(this.btnApply);
+            this.MinimumSize = new System.Drawing.Size(370, 280);
+            this.Name = "Form1";
+            this.Text = "LI2 Mod Manager (v1)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.CheckedListBox clbPatches;
+        private System.Windows.Forms.RichTextBox rtbDescription;
+    }
+}
+
---
+++ LegoIsland2Patcher/Form1.cs
@@ -0,0 +1,565 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace LegoIsland2Patcher
+{ 
+
+    public partial class Form1 : Form
+    {
+        bool foundExe = false;
+        List<modinfo> mods = new List<modinfo>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void populateModList()
+        {            
+            //Read mods folder
+            if (Directory.Exists("mods"))
+            {
+                string[] folders = Directory.GetDirectories("mods");
+
+                foreach (string f in folders)
+                {
+                    //Create Mod
+                    modinfo mi = new modinfo();
+                    mi.location = f;
+                    mi.name = f.Split('/', '\\')[f.Split('/', '\\').Length - 1]; //Default name is folder's name
+                    mi.author = "Unknown";
+                    mi.description = "No description available.";
+
+                    //Find info.txt
+                    if (File.Exists(f + "/info.txt"))
+                    {
+                        StreamReader reader = new StreamReader(f + "/info.txt");
+
+                        mi.name = reader.ReadLine();
+                        mi.author = reader.ReadLine();
+
+                        if (!reader.EndOfStream)
+                        {
+                            mi.description = "";
+                        }
+
+                        while (!reader.EndOfStream)
+                        {
+                            mi.description += reader.ReadLine() + "\n";
+                        }
+                    }
+
+                    //Add check box
+                    mods.Add(mi);
+                    clbPatches.Items.Add(mi.name + " - " + mi.author);
+
+                    //if installed file exists, check box
+                    bool check = false;
+
+                    if (File.Exists(f + "/installed"))
+                    {
+                        check = true;
+                    }
+                    else
+                    {
+                        //Check if file is already patched
+                        if (File.Exists(f + "/patch.txt"))
+                        {
+                            StreamReader sr = new StreamReader(f + "/patch.txt");
+
+                            //Read file to patch
+                            string file = "LEGO Island 2.exe"; //Default file to patch
+                            
+                            /*
+                            //Detect if first line is actually a patch line
+                            string tempfile = sr.ReadLine();
+                            if (tempfile.Split(';').Length <= 1) {
+                                file = tempfile;
+                            }
+                            else
+                            {
+                                sr.BaseStream.Position = 0;
+                                sr.DiscardBufferedData();
+                            }
+                            */
+
+                            if (File.Exists(file))
+                            {
+                                byte[] virtualFile = File.ReadAllBytes(file);
+
+                                while (!sr.EndOfStream && check == false)
+                                {
+                                    string raw = sr.ReadLine();
+
+                                    //Don't bother checking if not in the right format. Prevents errors too.
+                                    if (raw.Split(';').Length >= 2)
+                                    {
+                                        long offset = HexToLong(raw.Split(';')[0]);
+                                        byte[] patch = ToBytes(raw.Split(';')[1]);
+
+                                        int count = 0;
+                                        bool patchmatch = true;
+
+                                        check = true;
+                                        foreach (byte b in patch)
+                                        {
+                                            if (virtualFile[offset + count] != b)
+                                            {
+                                                patchmatch = false;
+                                                break;
+                                            }
+                                            count += 1;
+                                        }
+
+                                        check = patchmatch;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                check = false;
+                            }
+                        }
+                    }
+
+                    clbPatches.SetItemChecked(clbPatches.Items.Count - 1, check);
+                }
+            }
+        }
+
+
+        private void programStart()
+        {
+            if (File.Exists("LEGO Island 2.exe"))
+            {
+                foundExe = true;               
+            }
+
+            if (foundExe == true)
+            {
+                //Backup folder setup
+                if (!Directory.Exists("backup"))
+                {
+                    Directory.CreateDirectory("backup");
+                    byte[] b = new byte[0];
+                    File.WriteAllBytes("backup/_Do Not Delete This Folder", b);
+                }
+
+                //Load mods
+                populateModList();
+            }
+        }
+
+
+        private long HexToLong(string hex)
+        {
+            hex = hex.Trim();
+
+            long result = 0;
+            char[] hexchars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+            int position = 0;
+            for (int i = hex.Length - 1; i >= 0; i--)
+            {
+                int convert = 0;
+
+                for (int place = 0; place < 16; place++)
+                {
+                    if (Char.ToLower(hex[i]) == hexchars[place])
+                    {
+                        convert = place;
+                        place = 16;
+                    }
+                }
+
+                result += convert * (int)Math.Pow(16, position);
+
+                position += 1;
+            }
+
+            return result;
+        }
+
+
+        private byte[] ToBytes(string raw)
+        {
+            raw = raw.Trim();
+
+            if (raw == "")
+            {
+                return null;
+            }
+            else
+            {
+                if (raw.Length % 2 != 0)
+                {
+                    raw = '0' + raw;
+                }
+
+                int numofbytes = raw.Length / 2;
+                byte[] result = new byte[numofbytes];
+
+                for (int i = 0; i < raw.Length; i += 2)
+                {
+                    result[i / 2] = (byte)HexToLong(Char.ToString(raw[i]) + Char.ToString(raw[i + 1]));
+                }
+
+                return result;
+            }
+        }
+
+
+        private void installPatch(string location)
+        {
+            if (File.Exists(location + "/patch.txt"))
+            {
+                //Read patch info
+                StreamReader sr = new StreamReader(location + "/patch.txt");
+
+                string file = "LEGO Island 2.exe";
+
+                /*
+                //Check if first line is a filename or a patch line                
+                string tempfile = sr.ReadLine();
+                if (tempfile.Split(';').Length <= 1) {
+                    file = tempfile;
+                }
+                else
+                {
+                    sr.BaseStream.Position = 0;
+                    sr.DiscardBufferedData();
+                }
+                */
+                
+                if (File.Exists(file))
+                {
+                    //Read all patches
+                    while (!sr.EndOfStream)
+                    {
+                        string raw = sr.ReadLine();
+                        string[] parts = raw.Split(';');
+
+                        //Cannot continue unless patch is in the right format
+                        //todo: Display a message saying it's not correct
+                        if (parts.Length >= 2)
+                        {
+                            long offset = HexToLong(parts[0]);
+                            byte[] patch = ToBytes(parts[1]);
+                            byte[] originals = null;
+
+                            if (parts.Length >= 3)
+                            {
+                                originals = ToBytes(parts[1]);
+                            }
+
+                            //Check if patch is valid
+                            bool validpatch = false;
+                            if (originals == null)
+                            {
+                                validpatch = true;
+                            }
+                            else
+                            {
+                                byte[] virtualFile = File.ReadAllBytes("backup/" + file);
+
+                                int count = 0;
+                                bool originalmatch = true;
+
+                                foreach (byte o in originals)
+                                {
+                                    if (virtualFile[offset + count] != o)
+                                    {
+                                        originalmatch = false;
+                                        break;
+                                    }
+                                    count += 1;
+                                }
+
+                                validpatch = originalmatch;
+                            }
+
+                            //Apply patch
+                            if (validpatch == true)
+                            {
+                                byte[] virtualFile = File.ReadAllBytes(file);
+
+                                int count = 0;
+                                foreach (byte p in patch)
+                                {
+                                    virtualFile[offset + count] = p;
+                                    count += 1;
+                                }
+
+                                File.WriteAllBytes(file, virtualFile);
+                            }
+                        }
+                    }
+                }
+
+            }            
+        }
+
+        private void uninstallPatch(string location)
+        {
+            
+        }
+
+
+        private void modFileInstall(string dir)
+        {
+            /*Steps
+             * Check if bob/bod files are still present
+             * If they are not, backup old files, but only if not already backed up
+             * Install new files
+             */
+            
+            bool backupFile = true;
+
+            string[] parts = dir.Split('\\');
+
+            string[] partsagain = dir.Split('/', '\\');
+            if (partsagain[2] == "_data" && partsagain.Length >= 4)
+            {
+                string testbob = "_data/" + partsagain[3];
+
+                if (File.Exists(testbob + ".bob") && File.Exists(testbob + ".bod"))
+                {
+                    backupFile = false;
+                }
+            }
+                           
+            //If directory exists in the LI2 installation
+            if (Directory.Exists(dir.Split('/')[1]))
+            {
+                //Create directory in backup folder
+                if (backupFile == true)
+                {
+                    if (!Directory.Exists("backup/" + dir.Split('/')[1]))
+                    {
+                        Directory.CreateDirectory("backup/" + dir.Split('/')[1]);
+                    }
+                }
+            }
+            else
+            {
+                //If still using bod/bob files, create new folder                
+                Directory.CreateDirectory(dir.Split('/')[1]);
+            }
+
+            //Install files in directory
+            string[] files = Directory.GetFiles(dir);
+            foreach (string f in files)
+            {
+                parts = f.Split('/');
+
+                if (backupFile == true)
+                {
+                    //Backup file if not already backed up
+                    if (File.Exists(parts[1]) && !File.Exists("backup/" + parts[1]))
+                    {                           
+                        File.WriteAllBytes("backup/" + parts[1], File.ReadAllBytes(parts[1]));
+                    }
+                }
+                //Install file
+                File.WriteAllBytes(parts[1], File.ReadAllBytes(f));
+            }
+
+            //Search inner folders
+            string[] dirs = Directory.GetDirectories(dir);
+            foreach (string s in dirs)
+            {
+                modFileInstall(s);
+            }
+        }
+
+        private void modFileUninstall(string dir)
+        {
+            bool backupFile = true;
+
+            string[] parts = dir.Split('\\');
+            string[] partsagain = dir.Split('/', '\\');
+
+            if (partsagain[2] == "_data" && partsagain.Length >= 4)
+            {                
+                string testbob = "_data/" + partsagain[3];
+
+                if (File.Exists(testbob + ".bob") && File.Exists(testbob + ".bod"))
+                {                    
+                    backupFile = false;
+                }  
+            }
+
+            string[] files = Directory.GetFiles(dir);
+
+            foreach (string f in files)
+            {
+                parts = f.Split('/');
+
+                if (backupFile == true)
+                {
+                    if (File.Exists("backup/" + parts[1]) )
+                    {
+                        File.WriteAllBytes( parts[1], File.ReadAllBytes("backup/" + parts[1]) );
+                        File.Delete( "backup/" + parts[1] );
+                    }
+                }
+                else
+                {
+                    //Just delete if bob/bod files exist
+                    if (File.Exists(parts[1]))
+                    {                        
+                        File.Delete(parts[1]);
+                    }                    
+                }
+            }
+
+
+            //Search inner folders
+            string[] dirs = Directory.GetDirectories(dir);
+            foreach (string s in dirs)
+            {
+                modFileUninstall(s);
+            }
+            
+            //Remove empty folders
+            parts = dir.Split('\\', '/');
+
+            if (parts[0] == "mods")
+            {
+                string installPath = "";
+                for (int i = 0; i < parts.Length; i++)
+                {
+                    if (i >= 2)
+                    {
+                        installPath += parts[i] + "/";
+                    }
+                }
+
+                //First run: Delete installed directory
+                //Second run: Delete backedup directory
+                for (int i = 0; i < 2; i++)
+                {
+                    if (Directory.Exists(installPath))
+                    {
+                        if (Directory.GetDirectories(installPath).Length == 0 && Directory.GetFiles(installPath).Length == 0)
+                        {
+                            Directory.Delete(installPath);
+                        }
+                    }
+                    installPath = "backup/" + installPath;
+                }
+            }
+           
+        }
+
+
+
+        private void installMod(string location)
+        {
+            if (Directory.Exists(location + "/_data"))
+            {
+                modFileInstall(location + "/_data");
+            }
+            
+            if (!File.Exists(location + "/installed"))
+            {
+                byte[] b = new byte[0];
+                File.WriteAllBytes(location + "/installed", b);
+            }
+        }
+
+        private void uninstallMod(string location)
+        {
+            if (File.Exists(location + "/installed"))
+            {
+                if (Directory.Exists(location + "/_data"))
+                {
+                    modFileUninstall(location + "/_data");
+                }
+
+                File.Delete(location + "/installed");
+            }
+        }
+
+
+
+        private void applyMods()
+        {
+            //Backup exe
+            if (!File.Exists("backup/LEGO Island 2.exe"))
+            {
+                File.WriteAllBytes("backup/LEGO Island 2.exe", File.ReadAllBytes("LEGO Island 2.exe"));
+            }
+
+            //Reset
+            File.WriteAllBytes("LEGO Island 2.exe", File.ReadAllBytes("backup/LEGO Island 2.exe"));
+
+            //Loop through check boxes
+            for (int i = 0; i < clbPatches.Items.Count; i++)
+            {
+                if (clbPatches.GetItemChecked(i) == true)
+                {
+                    installPatch(mods[i].location);
+                    installMod(mods[i].location);
+                }
+                else
+                {
+                    uninstallPatch(mods[i].location);
+                    uninstallMod(mods[i].location);
+                }
+            }
+
+            MessageBox.Show("Done");
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            programStart();
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            applyMods();
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("http://www.rockraidersunited.com/");
+            Process.Start(sInfo);
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("http://www.github.com/JeffRuLz/LI2-Mod-Manager");
+            Process.Start(sInfo);
+        }
+
+        private void clbPatches_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            rtbDescription.Text = mods[clbPatches.SelectedIndex].description;
+        }
+
+    }
+
+    public class modinfo
+    {
+        public string name;
+        public string author;
+        public string description;
+        public string location;
+    }
+}
